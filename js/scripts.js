b124f350b1fc16beff617fb2552400658a4d4fef
unknown <aliu@CVDV3N298.gp.cv.commvault.com>
2015-08-11 11:57:24 -0400

    fixed smooth navigation

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,12 @@
 $(document).ready(function() {
     $('.main-content').fadeIn(800);
 });
+
+var padding = 30;
+$('.navbar-nav li a').bind('click', function (event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - padding
+    }, 1000, 'easeInOutExpo');
+    event.preventDefault();
+});
