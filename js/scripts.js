9851ad2c8b0f8fc58181e62ca9c9e61a10dfa70e
unknown <aliu@CVDV3N298.gp.cv.commvault.com>
2015-08-12 10:52:01 -0400

    adding smooth transitions to down arrow

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,18 @@ $(document).ready(function() {
 });
 
 /*for smooth navigation*/
-var padding = 30;
+var padding = 0;
 $('.navbar-nav li a').bind('click', function (event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - padding
+    }, 1000, 'easeInOutExpo');
+    event.preventDefault();
+});
+
+/*for smooth navigation*/
+var padding = 0;
+$('.scroll-down a').bind('click', function (event) {
     var $anchor = $(this);
     $('html, body').stop().animate({
         scrollTop: $($anchor.attr('href')).offset().top - padding
